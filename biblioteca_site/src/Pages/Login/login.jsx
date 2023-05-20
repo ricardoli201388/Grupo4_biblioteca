deab34249213366d3205151a32f6be21f03f46bf
Ricardo Li <ricardoli201388@gmail.com>
2023-05-19 22:24:24 -0300

    Estilização da página Login

--- biblioteca_site/src/Pages/Login/login.jsx
+++ biblioteca_site/src/Pages/Login/login.jsx
@@ -1,10 +1,18 @@
-import { Container, Titulo } from "./styles";
+import { Container, Titulo, Caixa_texto, Texto } from "./styles";
 
 function Login(){
 
 return (
     <Container>
         <Titulo>Login</Titulo>  
+        <Texto>Nome do usuário:</Texto>
+    <Caixa_texto>
+        <input></input>
+    </Caixa_texto>
+        <Texto>Senha:</Texto>
+    <Caixa_texto>
+        <input></input>
+    </Caixa_texto>
     </Container>
 )
 }
--- biblioteca_site/src/Pages/Login/styles.js
+++ biblioteca_site/src/Pages/Login/styles.js
@@ -6,10 +6,24 @@ export const Container = styled.div`
   align-content: center;
   width: 100%;
   height: 100%;
-  background-color: black;
+  background-color: #28846C;
 `;
 
 export const Titulo = styled.p`
-  font-size: 100px;
-  color: green;
+  margin-top: 5%;
+  margin-left: 20%;
+  font-size: 40px;
+  color: white;
 `;
+
+export const Caixa_texto = styled.div`
+  margin-left: 30%;
+  margin-right: 30%;
+  border: solid;
+  background-color: white;
+`
+export const Texto = styled.p`
+  margin-left: 30%;
+  font-size: 20px;
+  color: white;
+`
