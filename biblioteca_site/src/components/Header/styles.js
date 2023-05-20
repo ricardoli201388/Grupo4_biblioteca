0e66e29474c9e30cf40ab754c4ada375ab105e80
eduardobiagini <eduardobiagini1@gmail.com>
2023-05-20 16:17:50 -0300

    alterações header + footer (npm add react-icons)

--- biblioteca_site/src/components/Header/styles.js
+++ biblioteca_site/src/components/Header/styles.js
@@ -2,19 +2,32 @@ import styled from"styled-components";
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: row;
   align-content: center;
   width: 100%;
-  height: auto;
+  height: 80px;
   background-color: #0E5A46;
   justify-content: space-between;
+  box-shadow: 0 0 20px 3px;
+
+  > svg {
+    position: fixed;
+    color: white;
+    width: 30px;
+    height: 30px;
+    margin-top: 23px;
+    margin-left: 27px;
+    cursor: pointer;
+
+
+}
+
 `;
 
 export const Logo = styled.img`
     height: 50px;
-    width: 250px;
-    margin: 20px;
-    
+    width: 270px;
+    margin-top: 15px;
+    margin-left: 100px;
 
     /* @media (max-width: 700px) {
     width: 40px;
@@ -26,18 +39,19 @@ export const DivHeader = styled.div`
   display: flex;
   flex-direction: row;
   font-size: 50px;
-  color: red;
-  height: auto;
-  width: auto;
+  color: white;
+  height: 80px;
+  width: 300px;
+
 `;
 
 export const Text = styled.p`
   font-size: 23px;
   color: white;
   font-family: "poppins", sans-serif;
-  margin-top: 33px;
-  padding-right: 20px ;
-  
+  margin-right: 20px;
+  margin-top: 25px;
 `;
+
 
 
